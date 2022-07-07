aaecbcdec1aaad9a021cd07c5bb5a38da4974c53
annaaostrovvska <aloxnesir@gmail.com>
2022-07-07 16:25:50 +0300

    Final commit(2)

--- js/task-08.js
+++ js/task-08.js
@@ -10,6 +10,7 @@ function crtBoxs(){
     let staticWidth = '30px'
     let staticHeight = '30px'
     const div = document.createElement('div')
+    div.classList.add('box')
     for(let i = 0;i < input.value; i += 1){
     const div_clone = div.cloneNode(true)
     div_clone.style.width = staticWidth
@@ -17,6 +18,10 @@ function crtBoxs(){
     div_clone.style.backgroundColor = 'red'
     divBoxes.append(div_clone)
     }
-    console.log(input.value);
-    console.log(div_clone);
+}
+function dstBoxs(){
+    const allBoxes = document.querySelectorAll('.box')
+    allBoxes.forEach((el) => {
+        el.remove()
+    })
 }
